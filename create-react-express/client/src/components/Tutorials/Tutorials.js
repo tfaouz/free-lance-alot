e5a925a4bcf433f65ac12fa5c04a27b11abfe159
tfaouz <thibaultfaouz@gmail.com>
2019-07-04 16:50:00 -0400

    tutorial page done

--- create-react-express/client/src/components/Tutorials/Tutorials.js
+++ create-react-express/client/src/components/Tutorials/Tutorials.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 //import { Link } from 'react-router-dom';
 import "./Tutorials.css";
-import Button from 'react-bootstrap/Button';
+
 
 
 // depending on the current path, this component sets the "active" className on the appropriate navigation link item
@@ -10,67 +10,56 @@ const Tutorials = props =>
     <div className="container">
         <div className="featured">
             <div className="title">
-                <h3>Brush up on or learn new some new material.</h3>
+                <h3>Coding Tutorials.</h3>
                 <hr></hr>
             </div>
-            {/*---- Tutorial 1----- */}
             <div className="row">
                 <br></br>
                 <h4>freeCodeCamp</h4>
-                <p>We are looking for a remote US based full-stack Javascript developer with experience using
-                React, Node.JS, Socket.io, and AWS.
-                You'll be adding exciting new features to our live chat and chatbot platform (getgobot.com) and expanding integrations with third-party platforms like Shopify and Facebook. Experience with Shopify and Facebook API's is a plus. You will be working directly with both Co-Founders and the CTO.
-            </p>
+                <p>The languages and technologies currently taught by freeCodeCamp include HTML5, CSS 3, JavaScript, jQuery, Bootstrap, Sass, React.js, Node.js, Express.js, MongoDB, and Git.
+                </p>
+                <p><a href="https://www.freecodecamp.org">https://www.freecodecamp.org</a></p>
             </div>
-
-            {/* -------Tutorial 2 --------*/}
-            <br></br>
             <div className="row">
-                <h4>Web Designer</h4>
-                <p>Intersect Digital is looking for a professional Web Designer with experience in content management systems to take over the graphic and user interface design of clients' website. We're looking for a candidate who has created and maintained functional, attractive websites.
-                Incorporate changes recommended by the clients into the final design
-    Create asset building template files for front-end developers or junior designers to use
-    Implement and maintain high-quality UX best practices and incorporate them with content produced for the website - Create mock-ups for proposed project pages and update them as needed
-    Requirements:
-
-    Bachelor's in Computer Science, Graphic Design or related field, or 3+ years' additional experience.
-    The ability to work with minimal supervision, while delivering meticulous designs;
-    Advanced Photoshop and Illustrator skills.
-    Strong vision for mobile Internet consumer products, clear understanding of usability, user-centered design, accessibility and web standards.
-    Professional portfolio with at least 5 designs for internal review.
-    Must be comfortable working in a quick paced organization.
-    Applicants must be authorized to work in the United States without the need for a sponsor.
-    Compensation:
-
-    $150 per hour (fixed price)</p>
-
-           
                 <br></br>
-                {/* -------JOB 3 section ----------- */}
-                <div class="row">
-                    <h4>Front-end developer to assist on web app (HTML, CSS)</h4>
-                    <br></br>
-                    <hr></hr>
-                    <p className="description">We are looking for a front-end developer to help us finish up a web application. The application is developed in .net core,
-        and the client portion is a mix of custom javascript, Kendo UI controls,
-        and typescript. You would work with our lead web developer to implement pages based on existing templates and wire-frames. An example would be vendor page, where a user can add/edit vendor's in the system. The lead developer would be available to help with any questions, as well as review any code as necessary.
-
-        We are looking for someone we can send our mockups\wireframes and existing CSS. We would expect a 48 hr turn around which should include HTML file and any related CSS.
-
-        If successful, we will send you the remaining 14 pages we would like to have created in this manner. So we know you read the description and understand, please type "I'm available" in your response.
-
-        You should be comfortable with the following technologies:
-
-        HTML
-CSS</p>
-                  
-                </div>
+                <h4>Codecadamy</h4>
+                <p>Codecadamy teaches coding basics like HTML & CSS, JavaScript, PHP, Python, Ruby, Angularjs, The Command Line, and more </p>
+                <p><a href="https://www.codecademy.com/">https://www.codecademy.com/</a></p>
+            </div>
+            <div className="row">
+                <br></br>
+                <h4>Coursera</h4>
+                <p> Coursera is a free online learning tool for everyone who has an account registered. All courses offered by Coursera are “accessible for free”. Each course includes short video lectures on different topics and assignments to be submitted, usually on a weekly basis. </p>
+                <p><a href="https://www.coursera.org/">https://www.coursera.org/</a></p>
+            </div>
+            <div className="row">
+                <br></br>
+                <h4>edX</h4>
+                <p>EdX is an open-source higher education program governed by MIT and Harvard, making it another high-caliber resource where you can learn to code for free online. The site offers 107 great courses under the “computer science” category, teaching various coding languages. </p>
+                <p><a href="http://www.edx.org">http://www.edx.org</a></p>
+            </div>
+            <div className="row">
+                <br></br>
+                <h4>Khan Academy</h4>
+                <p> Khan Academy is a nonprofit with the mission to provide a free, world-class education for anyone, anywhere.</p>
+                <p><a href="https://www.khanacademy.org/">https://www.khanacademy.org/</a></p>
+            </div>
+            <div className="row">
+                <br></br>
+                <h4>Code.org</h4>
+                <p>Code.org provides learning materials specifically dedicated to increasing the rates of female and minority students entering computer science careers. Their free coding courses are designed for K-12 students, but can be useful to all ages. </p>
+                <p><a href="https://code.org/">https://code.org/</a></p>
+            </div>
+            <div className="row">
+                <br></br>
+                <h4>W3Schools</h4>
+                <p> W3Schools is the world’s largest web developer site, offering free coding tutorials and reference materials for learning just about every aspect of web programming. They offer HTML, CSS, JavaScript, SQL, PHP, jQuery, and more</p>
+                <p><a href="https://www.w3schools.com/">https://www.w3schools.com/</a></p>
             </div>
         </div>
-        
+    </div>
 
 
+export default Tutorials;
 
 
-
-        export default Tutorials;
--- create-react-express/client/src/pages/Tutorials.js
+++ create-react-express/client/src/pages/Tutorials.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-//import Navbar from "../components/Navbar/Navbar"
-import Footer from "../components/Footer/Footer"
-import Tutorials from "../components/Tutorials/Tutorials"
+import Navbar from "../components/Navbar/Navbar";
+import Header from "../components/Header/Header";
+import Categories from "../components/Categories/Categories";
+import Instructions from "../components/Instructions/Instructions";
+import Devs from "../components/Devs/Devs";
+import Footer from "../components/Footer/Footer";
+import Form from "../components/Form/Form"
+import Featuredjobs from "../components/Featuredjobs/Featuredjobs"
 
-function LearningTutorials() {
+
+function Home() {
   return (
-    <div>
-      <Tutorials />
-      <Footer/>
+    <div className="text-center animated fadeIn 3s">
+      <Header backgroundImage="https://i.imgur.com/lR3Nc9S.jpg">
+        <div className="animated bounceInUp"><h1>Freelance-A-Lot®</h1></div>
+      </Header>
+      <Navbar />
+      <Categories/>
+      <Instructions/>
+      <Devs/>
+      <Featuredjobs />
+     
+     <Footer />
     </div>
-  )
+  );
 }
 
-export default LearningTutorials;
+export default Home;
