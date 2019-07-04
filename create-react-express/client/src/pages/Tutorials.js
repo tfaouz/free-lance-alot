0cee803a95eef4b9a0484f6ee7c4db192bf303c4
tfaouz <thibaultfaouz@gmail.com>
2019-07-04 16:51:44 -0400

    tutorial more work done

--- create-react-express/client/src/pages/Tutorials.js
+++ create-react-express/client/src/pages/Tutorials.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from "../components/Navbar/Navbar";
-import Header from "../components/Header/Header";
-import Categories from "../components/Categories/Categories";
-import Instructions from "../components/Instructions/Instructions";
-import Devs from "../components/Devs/Devs";
-import Footer from "../components/Footer/Footer";
-import Form from "../components/Form/Form"
-import Featuredjobs from "../components/Featuredjobs/Featuredjobs"
+//import Navbar from "../components/Navbar/Navbar"
+import Footer from "../components/Footer/Footer"
+import Tutorials from "../components/Tutorials/Tutorials"
 
-
-function Home() {
+function LearningTutorials() {
   return (
-    <div className="text-center animated fadeIn 3s">
-      <Header backgroundImage="https://i.imgur.com/lR3Nc9S.jpg">
-        <div className="animated bounceInUp"><h1>Freelance-A-Lot®</h1></div>
-      </Header>
-      <Navbar />
-      <Categories/>
-      <Instructions/>
-      <Devs/>
-      <Featuredjobs />
-     
-     <Footer />
+    <div>
+      <Tutorials />
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default LearningTutorials;
